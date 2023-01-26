7dfde999d1013aae8aabba21ef9960625a0d08f0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-26 14:44:37 +0600

    Update2

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World.");
+    printf("Hello World.\n");
 
     int i = 20;
-    int p = 
+    int p = 30;
+
+    printf("Result:%d\n", i + p);
+    printf("%d\n", i * p);
 
    
     
