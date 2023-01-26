9b1e6fcbc9d37c8f2cea184faeb09799359d9d23
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-26 14:37:40 +0600

    Updated

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello world! i'm sumon. I'm from sylhet, Bangladesh. I'm best programmer.");
+    printf("Hello World.");
+
+    int i = 20;
+    int p = 
+
+   
+    
     
     return 0;
 }
