35f138e438319c70ff0a4f0d56c91fc006661277
sumon2003 <sumonkhanbd2003@gmail.com>
2023-02-04 16:56:16 +0600

    Updated1

--- helloworld.c
+++ helloworld.c
@@ -3,11 +3,19 @@
 int main(){
     int time = 20;
     if(time < 15){
-        printf("Good Day");
+        printf("Good Day\n");
     }else{
-        printf("Good Evening");
+        printf("Good Evening\n");
     }
-
+    
+    int mycash = 5000;
+    if(mycash < 3000){
+        printf("I have some money.\n");
+    }else if(mycash > 3000){
+        printf("I have thousand money.\n");
+    }else{
+        printf("There is no money.\n");
+    }
     
     return 0;
 }
