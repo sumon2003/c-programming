74a66ff35fe62fb694702d71f00699097b22fccc
sumon2003 <sumonkhanbd2003@gmail.com>
2023-02-08 16:51:18 +0600

    Updated

--- helloworld.c
+++ helloworld.c
@@ -30,7 +30,7 @@ int main(){
         printf("Tuesday\n");
         break;
         case 4:
-        printf("Thursday\n");
+        printf("Thursday");
         break;
         default:
         printf("Looking forward to the weekend\n");
