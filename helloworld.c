d8af47bdd9b814595ca73f9fc4e320023fc4c286
sumon2003 <sumonkhanbd2003@gmail.com>
2023-02-11 08:27:29 +0600

    Updated

--- helloworld.c
+++ helloworld.c
@@ -50,19 +50,24 @@ int main(){
     }
     while(i < 11);
 
-    int lower, upper, step;
+  /* int lower, upper, step;
     float fahr, celsius;
-    lower = 0; /* lower limit of temperature table */
-    upper = 300; /* upper limit */
-    step = 20; /* step size */
+    lower = 0; /* lower limit of temperature table *
+    upper = 300; /* upper limit *
+    step = 20; /* step size 
     fahr = lower;
 
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf("%4.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
-    }
-    
+    } */
+
+     int fahr;
+
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
 
 
     
