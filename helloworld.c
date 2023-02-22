24d27bf1c021dec91ac69c2e9e24b0f31023f88b
sumon2003 <sumonkhanbd2003@gmail.com>
2023-02-22 16:17:50 +0600

    Upgrade

--- helloworld.c
+++ helloworld.c
@@ -1,72 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int time = 20;
-    if(time < 15){
-        printf("Good Day");
-    }else{
-        printf("Good Evening");
-    }
     
-    int mycash = 5000;
-    if(mycash < 3000){
-        printf("I have some money.");
-    }else if(mycash > 3000){
-        printf("I have thousand money.");
-    }else{
-        printf("There is no money.");
-
-    }
-
-    int day;
-    printf("Enter your number:");
-    scanf("%d", &day);
+        int A, B, SOMA;
     
-    switch(day){
-        case 1:
-        printf("Monday");
-        break;
-        case 2:
-        printf("Tuesday");
-        break;
-        case 4:
-        printf("Thursday");
-        break;
-        default:
-        printf("Looking forward to the weekend");
-    }
-
-    int i = 0;
-
-    while(i < 5){
-        printf("%d\n", i);
-        i++;
-    }
-
-    
-    do{
-        printf("%d\n", i);
-        i++;
-    }
-    while(i < 11);
-
-  /* int lower, upper, step;
-    float fahr, celsius;
-    lower = 0; /* lower limit of temperature table *
-    upper = 300; /* upper limit *
-    step = 20; /* step size 
-    fahr = lower;
-
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%4.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    } */
-
-     int fahr;
-
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        scanf("%d%d", &A,&B);
+        SOMA = A + B;
+        printf("SOMA = %d\n", SOMA);
+        
 
 
 
