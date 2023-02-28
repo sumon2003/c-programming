3960fac27335a4e09da0d76eaed2a1084b2bb882
sumon2003 <sumonkhanbd2003@gmail.com>
2023-02-28 21:31:27 +0600

    Updated

--- helloworld.c
+++ helloworld.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
 int main(){
- int a, b, c, sum;
- float avg;
- printf("Enter three integer value:");
- scanf("%d%d%d",&a,&b,&c);
- sum = a+b+c;
- avg = sum/3;
- printf("\n The summation of three number is =%d", sum);
- printf("\nAverage of three number is=%.0f",avg);
- 
-
-
+ printf("My name is Md Sumon Hossain khan");
  return 0;
 }
