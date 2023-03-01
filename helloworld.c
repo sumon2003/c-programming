fde1b6565f6b6824e8730fa38f17b1a03636363e
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-01 23:17:21 +0600

    Update

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,6 @@ int main(){
 int A, B, C, D, difference;
 scanf("%d%d%d%d", &A,&B,&C,&D);
 difference=((A*B)-(C*D));
-printf("DIFERENCA = %d\n", difference);
+printf("DIFERENCA = %d", difference);
  return 0;
 }
