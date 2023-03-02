ac7318832eb7de2f8ce13cec70100d711d3541ec
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-02 23:32:34 +0600

    Upgrade

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-printf("My name is Sumon\n");
-printf("This is sumon\n");
-printf("Tis is not first day\n");
-printf("Today is thursday\n");
+printf("My name is\t Sumon\n");
+printf("This is\t sumon\n");
+printf("Tis is\t not first day\n");
+printf("Today is\t thursday\n");
  return 0;
 }
