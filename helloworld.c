bcba05be471c10b3a9195c7067306e1447754137
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-03 20:22:29 +0600

    Update

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main(){
-printf("My name is\t Sumon\n");
-printf("This is\t sumon\n");
-printf("Tis is\t not first day\n");
-printf("Today is\t thursday\n");
-printf("I'm bit tired.\n");
+int a;
+
  return 0;
 }
