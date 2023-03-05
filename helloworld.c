589a8168fdfd7753009dc488b0a3348bd07c3528
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-05 21:48:43 +0600

    Update

--- helloworld.c
+++ helloworld.c
@@ -5,7 +5,7 @@ int a;
 int b;
 scanf("%d%d", &a, &b);
 printf("Sum = %d\n", a+b);
-printf("Minus = %d", a-b);
+printf("Minus = %d\n", a-b);
 printf("Multiply = %d", a*b);
 printf("Divide = %d", a/b);
 
