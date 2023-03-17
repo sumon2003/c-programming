f6f1e1db6acf396726a1d31c6f6ca5ff86d24488
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-17 19:53:18 +0600

    Update

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,10 @@
-# include <stdio.h>
-    int main(){
-         int NUMBER,hours;
-         float amount,SALARY;
-         scanf("%d %d %f", &NUMBER, &hours, &amount);
-         SALARY = hours * amount;
-         printf("NUMBER = %d\n",NUMBER);
-         printf("SALARY = U$ %0.2f\n", SALARY);//SALARY = U$ 4100.00  
-         return 0;
+ #include<stdio.h>
+    int main()
+    {
+       double salary,value,TOTAL;
+      char name;
+      scanf("%s %lf %lf",&name,&salary,&value);                               
+      TOTAL = salary+value*.15;
+      printf("TOTAL = R$ %.2lf\n",TOTAL);
+      return 0; 
     }
