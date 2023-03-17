c4ff94796c301ac527f71e0f3cf403c8c9ee9fb1
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-17 19:44:12 +0600

    Updated

--- helloworld.c
+++ helloworld.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
-
-int main(){
-int a;
-int b;
-scanf("%d%d", &a, &b);
-printf("Sum = %d\n", a+b);
-printf("Minus = %d\n", a-b);
-printf("Multiply = %d\n", a*b);
-printf("Divide = %d", a/b);
-printf("Vagsesh = %d", a&b);
-
-
-
- return 0;
-}
+# include <stdio.h>
+    int main(){
+         int NUMBER,hours;
+         float amount,SALARY;
+         scanf("%d %d %f", &NUMBER, &hours, &amount);
+         SALARY = hours * amount;
+         printf("NUMBER = %d\n",NUMBER);
+         printf("SALARY = U$ %0.2f\n", SALARY);//SALARY = U$ 4100.00  
+         return 0;
+    }
