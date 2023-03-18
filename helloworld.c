a594b21ee6410c57ff9e1e63eee6cfdeecbd5ab7
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-18 21:00:49 +0600

    Update

--- helloworld.c
+++ helloworld.c
@@ -5,6 +5,6 @@
       char name;
       scanf("%s %lf %lf",&name,&salary,&value);                               
       TOTAL = salary+value*.15;
-      printf("TOTAL = R$ %.5lf",TOTAL);
+      printf("TOTAL = R$ %.2lf",TOTAL);
       return 0; 
     }
