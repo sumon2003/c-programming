72b0b126f87fd7c50c290503371cf5c5e225740f
sumon2003 <sumonkhanbd2003@gmail.com>
2023-03-19 22:20:02 +0600

    Update

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
  #include<stdio.h>
     int main()
     {
-       double salary,value,TOTAL;
+       
       char name;
       scanf("%s %lf %lf",&name,&salary,&value);                               
       TOTAL = salary+value*.15;
