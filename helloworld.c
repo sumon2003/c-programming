1e25709a1050ce71f1346ca4701784c08b0afbd7
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-19 15:27:33 +0600

    Area_tringle

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-int main() {
-	char upper,lower;
-	printf("Enter any upper case letter: ");
-	scanf("%c", &upper);
-    lower = tolower(upper);
-	printf("The lower case letter is: %c\n", lower);
-	return 0;
+#include<stdio.h> //
+int main(){
+	double a, b, c, s, area;
+	printf("Enter 3 numbers: ");
+	scanf("%lf%lf%lf",&a,&b,&c);
+	s = (a+b+c)/2;
+	area = sqrt(s*(s-a)*(s-b)*(s-c));
+	printf("The area of tringle is: %lf\n", area);
+
+
+return 0;
 }
 
