3bb813d1eba58ac2cf8a10af9f61f28a75c0c8c0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-01 14:52:43 +0600

    Fibonacci Series Using Array

--- helloworld.c
+++ helloworld.c
@@ -1,16 +1,20 @@
-#include<stdio.h> //
+#include<stdio.h> //Fibonacci Series
 int main(){
-	int i,n,sum=0;
-	printf("Enter N: ");
+	int array[50],n,i;
+	printf("How many Fibonacci numbers: ");
 	scanf("%d",&n);
-	int array1[n];
-	for(i=0; i<n; i++){
-		scanf("%d",&array1[i]);
+	array[0]=0;
+	array[1]=1;
+	for(i=2; i<n; i++)
+	{
+		array[i]=array[i-1]+array[i-2];
 	}
-	for(i=0; i<n; i++){
-		sum=sum+array1[i];
+	printf("\n");
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",array[i]);
 	}
-	printf("%d", sum);
+
 
 return 0;
 }
