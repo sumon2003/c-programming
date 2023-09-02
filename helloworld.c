5816da6c6d2fb60bbcba4146f6e169b8d42655f9
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-02 22:26:17 +0600

    Good Wishes to my friend

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <math.h>
-
 int main() {
-   int a, b, c, sum;
-   float average;
-   printf("Enter your value of a, b, c: ");
-   scanf("%d%d%d",&a,&b,&c);
-   sum = a + b + c;
-   average = (float)sum / 3;
-   printf("The sum of a+b+c = %d\n", sum);
-   printf("The average of a,b,c = %.2f\n",average);
+ int i,n;
+ printf("Tui jotobar chas toto bar n er value ta bosai dis.\nAmi chai n = ");
+ scanf("%d",&n);
+ for(i=0; i<=n; i++)
+ {
+    printf("Good job Suyab❤\n");
+ }
 return 0;
 }
