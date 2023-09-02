b22aea9e3072e6f790168f603ad62c8928ec103d
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-02 22:50:01 +0600

    Interval ( If, else if, else)

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main() {
- int i,n;
- printf("Tui jotobar chas toto bar n er value ta bosai dis.\nAmi chai n = ");
- scanf("%d",&n);
- for(i=0; i<=n; i++)
- {
-    printf("Good job Suyab❤\n");
- }
+float n;
+scanf("%f",&n);
+if (n>=0 && n<=25)
+   printf("Intervalo [0,25]\n");
+else if(n>25 && n<=50)
+   printf("Intervalo (25,50]\n");
+else if(n>50 && n<=75)
+   printf("Intervalo (50,75]\n");
+else if(n>75 && n<=100)
+   printf("Intervalo (75,100]\n");         
+else
+   printf("Fora de intervalo\n");
+ 
 return 0;
 }
