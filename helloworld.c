e456753aa05bb967e399494ed1c8c4cd2459f8a3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-02 19:12:30 +0600

    Bhaskara Formula

--- helloworld.c
+++ helloworld.c
@@ -1,27 +1,21 @@
-#include<stdio.h> //Linear Search
-int main(){
-	int num[]={10,20,30,40,50};
-	int value,position = -1,i;
-	printf("Enter the value you want to search: ");
-	scanf("%d", &value);
-	for(i=0; i<5; i++)
-	{
-		if(value==num[i])
-		{
-			position = i+1;
-			break;
-		}
-	}
-	if(position==-1)
-	{
-		printf("Item is not found.\n");
-	}
-	else
-	{
-		printf("The value is found at position %d.\n",position);
-	}
+#include <stdio.h>
+#include <math.h>
 
-
+int main() {
+    double a, b, c, root, X1, X2;
+    
+    scanf("%lf%lf%lf", &a, &b, &c);
+    
+    root = sqrt((b * b) - (4 * a * c));
+    
+    if (a == 0 || root < 0) {
+        printf("Impossivel calcular\n");
+    } else {
+        X1 = (-b + root) / (2 * a);
+        X2 = (-b - root) / (2 * a);
+        
+        printf("R1 = %.5lf\n", X1);
+        printf("R2 = %.5lf\n", X2);
+    }
 return 0;
 }
-
