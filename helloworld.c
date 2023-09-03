db583106d8233e46a8a8302e7e2ee73053587bc2
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-03 08:09:28 +0600

    Snack(BEE-1038)

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 int main() {
-int code_of_product1,code_of_product2, units_product1, units_product2;
-float price_product1, price_product2, amount_paid1, amount_paid2, total_paid;
-scanf("%d%d%f",&code_of_product1,& units_product1,&price_product1);
-scanf("%d%d%f",&code_of_product2,& units_product2,&price_product2);
-amount_paid1 = units_product1 * price_product1;
-amount_paid2 = units_product2 * price_product2;
-total_paid = amount_paid1 + amount_paid2;
-printf("VALOR A PAGAR: R$ %.2f\n", total_paid);
-
+int product_code, product_amount;
+float Cachorro_Quente, xSalada,xBacon,Torrada_simples, Refrigerante;
+scanf("%d%d",&product_code,&product_amount);
+Cachorro_Quente = product_amount * 4.00;
+xSalada = product_amount * 4.50;
+xBacon = product_amount * 5.00;
+Torrada_simples = product_amount * 2.00;
+Refrigerante = product_amount * 1.50;
+if(product_code == 1)
+   printf("Total: R$ %.2f\n", Cachorro_Quente);
+else if(product_code == 2)
+   printf("Total: R$ %.2f\n", xSalada);
+else if(product_code == 3)
+   printf("Total: R$ %.2f\n", xBacon);
+else if(product_code == 4)
+   printf("Total: R$ %.2f\n", Torrada_simples);
+else if(product_code == 5)
+   printf("Total: R$ %.2f\n", Refrigerante);            
 return 0;
 }
