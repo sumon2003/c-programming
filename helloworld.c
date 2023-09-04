1e3a3910b835130a16bacff8576e9cb7fe8fb914
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-04 19:17:29 +0600

    Divide chocklate

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-  double a,b, receive,total; 
-  char c[100];
-  scanf("%s %lf%lf", c,&a,&b);
-  receive = b * (15.0/100);
-  total = a + receive;
-  printf("TOTAL = R$ %.2lf\n",total); 
+int n,t;
+scanf("%d%d",&t,&n);
+if(n/2 && n%2==0)
+   printf("Yes\n");
+else
+   printf("No\n");   
 return 0;
 }
