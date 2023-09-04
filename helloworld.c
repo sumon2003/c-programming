7d797b0d0b492a7bc3156cc066603cab6a3895fa
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-04 19:30:38 +0600

    Messi vs Ronaldo

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int main(){
-int n,t;
-scanf("%d%d",&t,&n);
-if(n/2 && n%2==0)
-   printf("Yes\n");
-else
-   printf("No\n");   
+int A,B,X,Y,pntsMessi,pntsRonaldo;
+scanf("%d%d%d%d",&A,&B,&X,&Y);
+pntsMessi=A*2+B*1;
+pntsRonaldo=X*2+Y*1;
+if(pntsMessi>pntsRonaldo){
+  printf("Messi\n");
+}
+else if(pntsRonaldo>pntsMessi){
+  printf("Ronaldo\n");
+}
+else{
+  printf("Equal\n");
+}
+
 return 0;
 }
