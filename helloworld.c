46a45829c28a3adb321e145b15d09b1182973ea0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-06 07:28:40 +0600

    Codeforce 231A (Team)

--- helloworld.c
+++ helloworld.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 int main(){
-int T,a,b,c,i;
-scanf("%d", &T);
-for(i=0;i<T;i++){
-  scanf("%d%d%d",&a,&b,&c);
-  if(a+b==c || b+c==a || c+a==b){
-    printf("YES\n");
-  }
-  else{
-    printf("NO\n");
-  }
+int n,i,petya,vasya,tonya,count=0;
+scanf("%d",&n);
+for(i=0; i<n; i++){
+   scanf("%d%d%d",&petya,&vasya,&tonya);
+    if(petya==1 && vasya==1 && tonya==1)
+     count++;
+    if(petya==1 && vasya==1 && tonya==0)
+     count++;
+    if(petya==1 && vasya==0 && tonya==1)
+     count++;
+    if(petya==0 && vasya==1 && tonya==1)
+     count++;
+    if(petya==1 && vasya==0 && tonya==0)
+     continue;
+    if(petya==0 && vasya==1 && tonya==0)
+     continue;
+    if(petya==0 && vasya==0 && tonya==1)
+     continue;
+    if(petya==0 && vasya==0 && tonya==0)
+     continue;
+    
 }
-  
+printf("%d",count);
 return 0;
 }
