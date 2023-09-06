eefa2d505a186b679fff233a370ed4ad2beddeec
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-06 15:39:30 +0600

    Learning String

--- helloworld.c
+++ helloworld.c
@@ -1,27 +1,6 @@
 #include<stdio.h>
 int main(){
-int n,i,petya,vasya,tonya,count=0;
-scanf("%d",&n);
-for(i=0; i<n; i++){
-   scanf("%d%d%d",&petya,&vasya,&tonya);
-    if(petya==1 && vasya==1 && tonya==1)
-     count++;
-    if(petya==1 && vasya==1 && tonya==0)
-     count++;
-    if(petya==1 && vasya==0 && tonya==1)
-     count++;
-    if(petya==0 && vasya==1 && tonya==1)
-     count++;
-    if(petya==1 && vasya==0 && tonya==0)
-     continue;
-    if(petya==0 && vasya==1 && tonya==0)
-     continue;
-    if(petya==0 && vasya==0 && tonya==1)
-     continue;
-    if(petya==0 && vasya==0 && tonya==0)
-     continue;
-    
-}
-printf("%d",count);
+char s1[] = "Md Sumon Hossain Khan";
+printf("%s",s1);
 return 0;
 }
