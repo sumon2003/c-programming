115b6583a05406fd2547be2288f1537b65e6f3f1
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-08 09:45:39 +0600

    Array element reverse

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main(){
-int i,arr[10];
-for(i=0;i<10;i++){
-  printf("Elements %d = ",i);
-  scanf("%d",&arr[i]);
+int i,n;
+scanf("%d",&n);
+int arr[n];
+printf("The values store into the array are: \n");
+for(i=0;i<n;i++){
+   printf("element-%d: ",i);
+   scanf("%d",&arr[i]);
 }
-printf("Element in array are = ");
-for(i=0;i<10;i++){
-printf("%d ",arr[i]);
+printf("The values store into the array in reverse are: ");
+for(i=n-1; i>=0; i--){
+  printf("%d ",arr[i]);
 }
 return 0;
 }
