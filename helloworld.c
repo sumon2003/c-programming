ba0ef5245bd7476d109f8f5a2d4cb9f1713861bb
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-08 10:29:22 +0600

    Copy array to array1

--- helloworld.c
+++ helloworld.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 int main(){
-int i,n,sum=0;
-printf("Enter your index number: ");
+int i,n;
+printf("Enter your element number: ");
 scanf("%d",&n);
-int arr[n];
+int arr[100],arr2[100];
 printf("The values store into the array are: \n");
 for(i=0;i<n;i++){
    printf("element-%d: ",i);
    scanf("%d",&arr[i]);
 }
-printf("Sum of the array element: ");
-for(i=0; i<n; i++){
-   sum = sum + arr[i];
+printf("The elements stored in first array: ");
+for(i=0;i<n;i++){
+  printf("%d ",arr[i]);
 }
-printf("%d\n",sum);
+for(i=0;i<n;i++){
+  arr2[i] = arr[i];
+}
+printf("\nThe elements copied into the second array: ");
+for(i=0;i<n;i++){
+  printf("%d ",arr2[i]);
+}
+
 return 0;
 }
