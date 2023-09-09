08ffb430cbec3d19354853ab4cad120fa685418a
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-09 06:41:14 +0600

    Age in Days

--- helloworld.c
+++ helloworld.c
@@ -1,25 +1,12 @@
 #include<stdio.h>
 int main(){
-int i,n;
-printf("Enter your element number: ");
-scanf("%d",&n);
-int arr[100],arr2[100];
-printf("The values store into the array are: \n");
-for(i=0;i<n;i++){
-   printf("element-%d: ",i);
-   scanf("%d",&arr[i]);
-}
-printf("The elements stored in first array: ");
-for(i=0;i<n;i++){
-  printf("%d ",arr[i]);
-}
-for(i=0;i<n;i++){
-  arr2[i] = arr[i];
-}
-printf("\nThe elements copied into the second array: ");
-for(i=0;i<n;i++){
-  printf("%d ",arr2[i]);
-}
-
+int N,year,month,day;
+scanf("%d",&N);
+year = N/365;
+month = (N-(year*365))/30;
+day = N-((year*365)+(month*30));
+printf("%d years\n",year);
+printf("%d months\n",month);
+printf("%d days\n",day);
 return 0;
 }
