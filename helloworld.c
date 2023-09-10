d6c0a0f881a760fa7d18708631402337b0ad4ad0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-10 21:58:32 +0600

    Two intervals

--- helloworld.c
+++ helloworld.c
@@ -1,22 +1,42 @@
 #include<stdio.h>
 int main(){
-int a,b,c, sum,minus,multiply;
-char s,q;
-scanf("%d %c %d %c %d",&a,&s,&b,&q,&c);
-sum = a + b;
-minus = a - b;
-multiply = a * b;
-if(sum==c || minus==c || multiply==c){
-    printf("Yes\n");
-}   
-else if(sum!=c && s=='+'){
-    printf("%d",sum);
+int a,b,c,d;
+scanf("%d %d %d %d",&a,&b,&c,&d);
+if(c<b && d>b){
+    printf("%d %d\n",c,b);
 }
-else if(minus!=c && s=='-'){
-    printf("%d",minus);
+else if(c<b && d<b){
+    printf("%d %d\n",c,d);
 }
-else if(multiply!=c && s=='*'){
-    printf("%d",multiply);
+else if(c<b && b==d){
+    printf("%d %d\n",c,d);
+}
+else if(c<a && b==d){
+    printf("%d %d\n",a,d);
+}
+else if(c<a && d<b){
+    printf("%d %d\n",a,d);
+}
+else if(c<a && b<d){
+    printf("%d %d\n",a,b);
+}
+else if(a==c && b==d){
+    printf("%d %d\n",a,b);
+}
+else if(a==c && d<b){
+    printf("%d %d\n",c,d);
+}
+else if(a==c && b<d){
+    printf("%d %d\n",a,b);
+}
+else if(b==d && a>c){
+    printf("%d %d\n",a,b);
+}
+else if(c==b && b<d){
+    printf("%d %d\n",b,c);
+}    
+else if(c>b){
+    printf("-1\n");
 }
 
 return 0;
