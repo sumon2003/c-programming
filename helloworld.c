551adf2da9c40047b52464c63b2e9678ea9d1d80
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-15 07:05:44 +0600

    Sum with array

--- helloworld.c
+++ helloworld.c
@@ -1,43 +1,13 @@
 #include<stdio.h>
 int main(){
-int a,b,c,d;
-scanf("%d %d %d %d",&a,&b,&c,&d);
-if(c<b && d>b){
-    printf("%d %d\n",c,b);
+int n,i,sum=0;
+scanf("%d",&n);
+int arr[n];
+for(i=0; i<n; i++){
+    scanf("%d",&arr[i]);
+    sum = sum + arr[i];
 }
-else if(c<b && d<b){
-    printf("%d %d\n",c,d);
-}
-else if(c<b && b==d){
-    printf("%d %d\n",c,d);
-}
-else if(c<a && b==d){
-    printf("%d %d\n",a,d);
-}
-else if(c<a && d<b){
-    printf("%d %d\n",a,d);
-}
-else if(c<a && b<d){
-    printf("%d %d\n",a,b);
-}
-else if(a==c && b==d){
-    printf("%d %d\n",a,b);
-}
-else if(a==c && d<b){
-    printf("%d %d\n",c,d);
-}
-else if(a==c && b<d){
-    printf("%d %d\n",a,b);
-}
-else if(b==d && a>c){
-    printf("%d %d\n",a,b);
-}
-else if(c==b && b<d){
-    printf("%d %d\n",b,c);
-}    
-else if(c>b){
-    printf("-1\n");
-}
+printf("%d",sum);
 
 return 0;
 }
