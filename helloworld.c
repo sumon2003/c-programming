6b1a5c9da2b171e4e041b75bef94600e99e0ca29
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-15 08:49:37 +0600

    Minimum with array

--- helloworld.c
+++ helloworld.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main(){
-int n,i,max;
+int n,i,min;
 scanf("%d",&n);
 int arr[n];
 for(i=0;i<n;i++){
     scanf("%d",&arr[i]);
 }
-max=arr[0];
+min=arr[0];
 for(i=0;i<n;i++){
-    if(max<arr[i]){
-        max=arr[i];
+    if(min>arr[i]){
+        min=arr[i];
     }
 }
-printf("%d",max);
+printf("%d",min);
 return 0;
 }
