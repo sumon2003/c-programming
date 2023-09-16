4f7047c1cf5072799bbe63d3eca329224083dc44
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-16 08:15:38 +0600

    Check Even or Odd

--- helloworld.c
+++ helloworld.c
@@ -1,18 +1,15 @@
-//Write a program to find a character is vowel or not, if vowel print ‘Vowel’, if consonant print ‘Consonant’, otherwise print ‘Not an Alphabet’
+//Write a program to find a number is even or odd, if even print ‘EVEN’ if odd print ‘ODD’
 
 #include<stdio.h>
 int main()
 {
-   char x;
-   scanf("%c",&x);
-   if(x=='A' || x=='E' || x=='I' || x=='O' || x=='U' || x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
-    printf("Vowel");
-   }
-   else if((x >'a' && x<='z') || (x>'A' && x<='Z')){
-    printf("Consonant");
+   int num;
+   scanf("%d",&num);
+   if(num%2==0){
+    printf("EVEN\n");
    }
    else{
-    printf("Not an Alphabet");
+    printf("ODD\n");
    }
    
     return 0;
