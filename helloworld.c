9c0b1d49a9507a46c807fc95edfdeb2bcdb6992b
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-16 07:26:47 +0600

    maximum with array

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,21 @@
+//Given an array of 15 integers, find the maximum of the array solution
 #include<stdio.h>
-int main(){
-int n,i,value,sum=0;
-float avg;
-scanf("%d",&n);
-for(i=0;i<n;i++){
-    scanf("%d",&value);
-    sum = sum + value;
-}
-printf("Sum = %d\n",sum);
-avg = (float)sum/n;
-printf("Average = %.3f\n",avg);
-return 0;
-}
+int main()
+{
+
+    int arr[15],i,max;
+    for(i=0; i<15; i++){
+        scanf("%d",&arr[i]);
+    }
+    max = arr[0];
+    for(i=0; i<15; i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+    printf("%d\n",max);
+
+
+   
+    return 0;
+}    
