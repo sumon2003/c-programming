90fb1e56fbadfda81e37b2da9865b2044a211533
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-16 08:02:00 +0600

    Check vowel consonant or not alphabet

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,4 @@
-//Write a program to find a character is vowel or not, if vowel print ‘Yes’, otherwise print ‘No’
+//Write a program to find a character is vowel or not, if vowel print ‘Vowel’, if consonant print ‘Consonant’, otherwise print ‘Not an Alphabet’
 
 #include<stdio.h>
 int main()
@@ -6,10 +6,13 @@ int main()
    char x;
    scanf("%c",&x);
    if(x=='A' || x=='E' || x=='I' || x=='O' || x=='U' || x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
-    printf("Yes");
+    printf("Vowel");
+   }
+   else if((x >'a' && x<='z') || (x>'A' && x<='Z')){
+    printf("Consonant");
    }
    else{
-    printf("No");
+    printf("Not an Alphabet");
    }
    
     return 0;
