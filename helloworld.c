9b8cbab1faf0d802952381dbbff0a56b4436b84c
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-24 10:30:18 +0600

    Range

--- helloworld.c
+++ helloworld.c
@@ -1,25 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,n,i,count=0;
-    scanf("%d",&n);
-    for(i=0; i<n; i++){
-        scanf("%d%d%d",&a,&b,&c);
-        if(a==1 && b==1 && c==1){
-            count++;
-        }
-        if(a==1 && b==1 && c==0){
-            count++;
-        }
-        if(a==1 && b==0 && c==1){
-            count++;
-        }
-        if(a==0 && b==1 && c==1){
-            count++;
-        }
-        
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<b && a<c && b>a && b<c && c>a && c>b){
+        printf("Yes\n");
     }
-    printf("%d\n",count);
+    else{
+        printf("No\n");
+    }
 
    return 0;
 
