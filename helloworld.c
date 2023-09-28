a96a9b19da3cc38aeb090633626b582504610ab5
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-28 09:19:53 +0600

    Multiples

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-   int a,b;
-   scanf("%d %d",&a,&b);
-   if(a>=b){
-      printf("Yes\n");
-   }
-   else{
-      printf("No\n");
-   }
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if(a%b==0 || b%a==0){
+   printf("Multiples\n");
+  }
+  else{
+   printf("No Multiles\n");
+  }
 
    return 0;
 }
