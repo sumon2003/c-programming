5af4d94dff546e17c47a22da3f984f695b4981df
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-28 06:19:46 +0600

    Say hello with C++

--- helloworld.c
+++ helloworld.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
-    if(a<b && a<c && b>a && b<c && c>a && c>b){
-        printf("Yes\n");
-    }
-    else{
-        printf("No\n");
-    }
-
+   char S[10];
+   scanf("%s",S);
+   printf("Hello, %s\n",S);
    return 0;
 
 }
