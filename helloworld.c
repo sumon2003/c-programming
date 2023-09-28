d347b076acf106efddea88c6c238a2f4d2b155c0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-28 08:37:16 +0600

    Two numbers(Floor, Ceil, Round)

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main()
 {
-   long long n,sum;
-   scanf("%lld",&n);
-   sum = (n*(n+1))/2; //Summation from 1 to N
-   printf("%lld",sum);
+   float a,b,ab_floor,ab_ceil,ab_round;
+   scanf("%f %f",&a,&b);
+   ab_floor = floor(a/b);
+   ab_ceil = ceil(a/b);
+   ab_round = round(a/b);
+   printf("floor %.0f / %.0f = %.0f\n",a,b,ab_floor);
+   printf("ceil %.0f / %.0f = %.0f\n",a,b,ab_ceil);
+   printf("round %.0f / %.0f = %.0f\n",a,b,ab_round);
 
    return 0;
 }
