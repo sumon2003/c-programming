61c7050dbc52a5fd0da0c564fba092adc6f60a48
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-28 06:39:28 +0600

    Basic data types in C

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main()
 {
-   char S[10];
-   scanf("%s",S);
-   printf("Hello, %s\n",S);
+   int d;
+   long long l;
+   char c;
+   float f;
+   double dl;
+   scanf("%d %lld %c %f %lf",&d,&l,&c,&f,&dl);
+   printf("%d\n%lld\n%c\n%f\n%lf\n",d,l,c,f,dl);
+
    return 0;
 
 }
