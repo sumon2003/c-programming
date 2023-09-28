20196af32de1fcf467c8dde4a7e6bf91857a8779
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-28 08:43:50 +0600

    Welcome for you with Conditions

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-   float a,b,ab_floor,ab_ceil,ab_round;
-   scanf("%f %f",&a,&b);
-   ab_floor = floor(a/b);
-   ab_ceil = ceil(a/b);
-   ab_round = round(a/b);
-   printf("floor %.0f / %.0f = %.0f\n",a,b,ab_floor);
-   printf("ceil %.0f / %.0f = %.0f\n",a,b,ab_ceil);
-   printf("round %.0f / %.0f = %.0f\n",a,b,ab_round);
+   int a,b;
+   scanf("%d %d",&a,&b);
+   if(a>=b){
+      printf("Yes\n");
+   }
+   else{
+      printf("No\n");
+   }
 
    return 0;
 }
