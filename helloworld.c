d526a8d096ed1caf94084ed1b9c370f1f619479b
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-28 12:10:47 +0600

    The Brothers

--- helloworld.c
+++ helloworld.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-  int a,b,c,max,min;
-  scanf("%d %d %d",&a,&b,&c);
-  max = min = a;
-  if(b>max)
-   max = b;
-  if(b<min)
-   min = b;
-  if(c>max)
-   max = c;
-  if(c<min)
-   min = c;  
-  printf("%d %d\n",min,max);
+  char f1[100],s1[100],f2[100],s2[100];
+  scanf("%s %s %s %s",f1,s1,f2,s2);
+  
+  if(strcmp(s1,s2)==0){
+    printf("ARE Brothers\n");
+  }
+  else{
+    printf("NOT\n");
+  }
    return 0;
 }
