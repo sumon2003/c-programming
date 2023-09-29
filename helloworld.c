a9af28490a42fc89932194f4bf28dbbc6d045375
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-29 11:04:01 +0600

    Compare

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-  long long a,b,c,d,last_2_digit;
+  long long a,b,c,d;
+  
   scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
-  //lat 2 digit of a number = num % 100
-  last_2_digit = (((a*b)%100)*((c*d)%100))%100;
-  printf("%02lld\n",last_2_digit);
-
+  double pow1 = pow(a, b);
+  double pow2 = pow(c, d);
+  if(pow1 > pow2){
+    printf("YES\n");
+  }
+  else{
+    printf("NO\n");
+  }
   return 0;
 }
