3c428c12cec377aecf0b6d57a44c19703820c92a
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-29 08:25:25 +0600

    The last two digit

--- helloworld.c
+++ helloworld.c
@@ -1,21 +1,11 @@
 #include<stdio.h>
 int main()
 {
-  int a,b;
-  char s;
-  scanf("%d %c %d",&a,&s,&b);
-  if(a<b && s=='<'){
-    printf("Right");
-  }
-  else if(a>b && s=='>'){
-    printf("Right");
-  }
-  else if(a==b && s=='='){
-    printf("Right");
-  }
-  else{
-    printf("Wrong");
-  }
+  long long a,b,c,d,last_2_digit;
+  scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+  //lat 2 digit of a number = num % 100
+  last_2_digit = (((a*b)%100)*((c*d)%100))%100;
+  printf("%02lld\n",last_2_digit);
 
   return 0;
 }
