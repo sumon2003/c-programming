105f67ec6ef898e6560844f207b868b2e0eb6e8e
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-31 07:33:39 +0600

    C++[Single Line and Multy Line Comment]

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
+    // This is a Single line comment.
+    /*This is Multiline comment.
+      This is looking good.*/
    cout << "Hello World!" << endl<<endl;
    cout << "I am learning C++.";
     return 0;
