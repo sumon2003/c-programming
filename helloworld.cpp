43b0fdca8ac5869b2cc3020842603895a9d18d33
sumon2003 <sumonkhanbd2003@gmail.com>
2024-01-03 16:58:49 +0600

    Update Array

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,8 +3,31 @@ using namespace std;
 
 int main(){
 
-    cout << "Hello World" << endl ;
-    cout << "Sumon" << endl;
+int n;
+cout << "Enter array Size: ";
+cin >> n;
+
+int arr[n];
+cout << "Enter the array: ";
+for(int i=0; i<n; i++){
+    cin >> arr[i];
+}
+
+cout << "Before update: ";
+for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+}
+
+int value, update_index;
+cout << endl << "Enter Value and Update index: ";
+cin >> value >> update_index;
+
+arr[update_index] = value;
+ 
+cout << "After Update: ";
+for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+} 
 
 return 0;  
 }
