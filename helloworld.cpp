81c2872f704fbfef70d2287854d28aa113f0e73c
sumon2003 <sumonkhanbd2003@gmail.com>
2024-01-03 20:50:16 +0600

    Codechef Round

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,30 +4,9 @@ using namespace std;
 int main(){
 
 int n;
-cout << "Enter array Size: ";
-cin >> n;
-
-int arr[n];
-cout << "Enter the array: ";
-for(int i=0; i<n; i++){
-    cin >> arr[i];
-}
-
-cout << "Before update: ";
-for(int i=0; i<n; i++){
-    cout << arr[i] << " ";
-}
-
-int value, update_index;
-cout << endl << "Enter Value and Update index: ";
-cin >> value >> update_index;
-
-arr[update_index] = value;
- 
-cout << "After Update: ";
-for(int i=0; i<n; i++){
-    cout << arr[i] << " ";
-} 
+cin>>n;
+if(n==4) cout<<"YES"<<endl;
+else cout<<"NO"<<endl;
 
 return 0;  
 }
