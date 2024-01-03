ef49177b0cacebcb744158736582154db641310a
sumon2003 <sumonkhanbd2003@gmail.com>
2024-01-03 23:45:09 +0600

    Choklate Problem

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,10 +3,25 @@ using namespace std;
 
 int main(){
 
-int n;
-cin>>n;
-if(n==4) cout<<"YES"<<endl;
-else cout<<"NO"<<endl;
+int t,n;
+cin>>t;
+int max=0,min=0;
+for(int i=0; i<t; i++){
+    cin>>n;
+    if(n%2==0){
+        max = n;
+        min = n/2;
+    }else{
+        max = n;
+        min = (n/2)+1;
+    }
+    cout<<min<<" "<<max<<endl;
+}
+
+
+
+max = 0;
+min = 0;
 
 return 0;  
 }
