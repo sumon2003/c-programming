5d6f10760a49c4f4a725af9f4c8c3b9dd10b17b2
sumon2003 <sumonkhanbd2003@gmail.com>
2024-01-09 16:25:47 +0600

    Problem 6

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,20 +6,48 @@ using namespace std;
 
 int main(){
 
-    int t;
+    int a, b, c, r1, r2, r3, r4, r5;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    r1 = a + b * c;
+    r2 = a * (b + c);
+    r3 = a * b * c;
+    r4 = (a + b) * c;
+    r5 = a + b + c;
+
+     int maxResult = max({r1, r2, r3, r4, r5});
+     cout << maxResult;
+    
+    /*if (r1 > r2 && r1 > r3 && r1 > r4 && r1 > r5) cout << r1;
+    else if (r2 > r1 && r2 > r3 && r2 > r4 && r2 > r5) cout << r2;
+    else if (r3 > r1 && r3 > r2 && r3 > r4 && r3 > r5) cout << r3;
+    else if (r4 > r1 && r4 > r2 && r4 > r3 && r4 > r5) cout << r4;
+    else cout << r5;*/
+
+    
+return 0;  
+}
+
+/*
+int t;
     cin >> t;
     
     for(int i=0; i<t; i++)
     {   
-        int a, b, c, c1 = 0, c2 = 0;
-        cin >> a >> b >> c;
+        int n;
+        cin >> n;
 
-        c1 = a + b;
-        c2 = a - b;
-
-        if(c1 == c) cout << "+" << endl;
-        else if(c2 == c) cout << "-" << endl;
+        char str[n];
+        cin >> str;
+        
+        for(int i=0; i<n; i++)
+        {
+            if((str[i] == 0) && (str[i+1] == 0)) cout << "A";
+            else if((str[i] == 0) && (str[i+1] == 1)) cout << "T";
+            else if((str[i] == 1) && (str[i+1] == 0)) cout << "C";
+            else if((str[i] == 1) && (str[i+1] == 1)) cout << "G";
+        }
     }
-    
-return 0;  
-}
+*/
