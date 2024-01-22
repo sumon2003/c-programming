75e3b2536e1b800fc79800f90529d5a701bf67f4
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-22 20:48:23 +0600

    Strings

---
+++ strings.cpp
@@ -0,0 +1,28 @@
+/*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
+/*//   Md Sumon Hossain Khan   //*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1, str2, str3;
+
+    getline(cin, str1);
+    getline(cin, str2);
+
+    int len1 = str1.size();
+    int len2 = str2.size();
+
+    cout << len1 << " " << len2 << endl;
+    cout << str1 + str2 << endl;
+
+    str3[0] = str1[0];
+    str1[0] = str2[0];
+    str2[0] = str3[0];
+
+    cout << str1 << " " << str2 << endl;
+
+
+    return 0;
+}
